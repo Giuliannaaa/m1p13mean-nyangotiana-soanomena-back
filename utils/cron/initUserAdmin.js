2c6b2fbd1878de7b02bfe84e3bacc6594ce7fe5b
Ny Angotiana <ralambo.nyango@gmail.com>
2026-02-25 22:15:16 +0300

    Debug initUserAdmin function for prod

--- utils/cron/initUserAdmin.js
+++ utils/cron/initUserAdmin.js
@@ -1,18 +1,24 @@
 const User = require('../../models/User');
 
-const initUserAdmin = () => {
-    const user = User.findOne({ role: 'Admin' });
-    if (!user) {
-        User.create({
-            firstname: 'admin',
-            lastname: 'admin',
-            email: 'admin@admin.com',
-            password: 'admin123',
-            role: 'Admin'
-        });
-        console.log('User admin created');
+const initUserAdmin = async () => {
+    try {
+        const user = await User.findOne({ role: 'Admin' });
+
+        if (!user) {
+            await User.create({
+                firstname: 'admin',
+                lastname: 'admin',
+                email: 'admin@admin.com',
+                password: 'admin123',
+                role: 'Admin'
+            });
+            console.log(' User admin created');
+        } else {
+            console.log('User admin already exists');
+        }
+    } catch (error) {
+        console.error('Error in initUserAdmin:', error.message);
     }
-    console.log('User admin already exists');
-}
+};
 
 module.exports = initUserAdmin;
